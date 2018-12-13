41d67953aa3dabb0f69bcfb61887bd26cd9ed23a
Kattiie <45550120+Kattiie@users.noreply.github.com>
2018-12-12 19:50:28 -0500

    Add files via upload

---
+++ Class_code.m
@@ -0,0 +1,33 @@
+% clc;clear;
+% load Init4.mat;
+%%
+X = transpose(table2array(FermienergyinonepointDATA1));
+%X = X(2:end,:);
+%For Bulk Modulus
+Y = Y2.B;
+Y = Y/max(Y);
+%%
+YX = [Y X];
+
+YX = YX(randperm(size(YX,1)),:);
+
+Y = YX(:,1);
+cl = YX(:,2);
+X = YX(:,3:end);
+
+
+%Normalize Y
+%Y = Y./max(Y);
+
+
+% %Normalize DOS;
+X = X./max(X,[],2);
+%Mean center
+Mean_X = mean(X, 1);
+X = X - Mean_X;
+
+% 
+% [PCALoadings,PCAScores,PCAVar] = pca(X);
+
+
+Classification = [Y,cl,X];
